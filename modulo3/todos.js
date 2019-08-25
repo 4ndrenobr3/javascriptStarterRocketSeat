61b75337cd18494fd42a299de1cb0a64f2042fc4
nobre4ndr3 <nobre4ndr3@gmail.com>
2019-08-25 14:56:48 -0300

    Starting Application

--- modulo3/todos.js
+++ modulo3/todos.js
@@ -1,1 +1,9 @@
-alert('testando');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Aprender Javascript',
+    'Acessar Comunidade da rocketseat'
+];
